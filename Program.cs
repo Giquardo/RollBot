ded590b06ea4516441041642ed0e969ddd398e50
giquardo vandaele <giquardo@telenet.be>
2024-09-01 12:11:04 +0200

    Startup done

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.EventArgs;
+using Rollbot.Config;
+
+namespace RollBot;
+
+internal class Program
+{
+    private static DiscordClient Client { get; set; }
+    private static CommandsNextExtension Commands { get; set; }
+    static async Task Main(string[] args)
+    {
+        var jsonReader = new JSONReader();
+        await jsonReader.ReadJSON();
+
+        var discordConfig = new DiscordConfiguration()
+        {
+            Intents = DiscordIntents.All,
+            Token = jsonReader.token,
+            TokenType = TokenType.Bot,
+            AutoReconnect = true
+        };
+
+        Client = new DiscordClient(discordConfig);
+
+        Client.Ready += Client_Ready;
+
+        await Client.ConnectAsync();
+        await Task.Delay(-1);
+    }
+
+    private static Task Client_Ready(DiscordClient sender, ReadyEventArgs args)
+    {
+        return Task.CompletedTask;
+    }
+}
